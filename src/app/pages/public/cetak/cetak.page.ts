1e031d832c8cc2ee03f743220ca0122af4e7d0ed
mfnizam <fatikhunnizam@gmail.com>
2022-10-13 21:49:39 +0700

    add cetak page

--- src/app/pages/public/cetak/cetak.page.ts
+++ src/app/pages/public/cetak/cetak.page.ts
@@ -6,6 +6,7 @@ import { FungsiService } from '../master/fungsi/fungsi.service';
 import { Fungsi } from '../master/fungsi/fungsi.types';
 import { CetakService } from './cetak.service';
 import * as moment from 'moment';
+import { BerandaService, Kategori } from '../beranda/beranda.service';
 
 @Component({
   selector: 'app-cetak',
@@ -14,23 +15,26 @@ import * as moment from 'moment';
 export class CetakPage {
   jenis: string;
   loadingDataPermintaan = false;
-  dataPerminataan /* TODO: Add permintaan types */ = [];
+  dataPermintaan /* TODO: Add permintaan types */ = [];
 
   loadingDataSelectFungsi = false;
   dataSelectFungsi: Fungsi[] = [];
+  loadingDataSelectKategori = false;
+  dataSelectKategori: Kategori[] = this.beranda._kategori;
   loadingCetak = false;
 
-  formFilterCetakFungsi: FormGroup = this.formBuilder.group({
+  formFilterCetak: FormGroup = this.formBuilder.group({
     fungsi: [null, Validators.required],
     namaFungsi: [null],
-    tanggalStart: [null],
     kategori: [null],
     namaKategori: [null],
+    tanggalStart: [null],
     tanggalEnd: [null],
     tanggalCombine: [null]
   })
 
   selectedFungsi: Fungsi;
+  selectedKategori: Kategori;
   selectedDate: { from: string, to: string } = { from: null, to: null }
   maxDate: Date = new Date();
   minDate = new Date('1 1 2020');
@@ -40,7 +44,8 @@ export class CetakPage {
     private formBuilder: FormBuilder,
     private cetak: CetakService,
     private fungsi: FungsiService,
-    private loading: LoadingController
+    private loading: LoadingController,
+    private beranda: BerandaService
   ) {
     this.jenis = this.route.snapshot.params.jenis
   }
@@ -53,7 +58,7 @@ export class CetakPage {
         console.log(res)
         this.loadingDataSelectFungsi = false;
         this.dataSelectFungsi = res.fungsi;
-        this.selectedFungsi = res.fungsi.find(v => v._id == this.formFilterCetakFungsi.get('fungsi').value)
+        this.selectedFungsi = res.fungsi.find(v => v._id == this.formFilterCetak.get('fungsi').value)
       }, err => {
         console.log(err)
         this.loadingDataSelectFungsi = false;
@@ -62,7 +67,7 @@ export class CetakPage {
 
   selectFungsi(modal) {
     if (!this.selectedFungsi?._id) return;
-    this.formFilterCetakFungsi.patchValue({
+    this.formFilterCetak.patchValue({
       fungsi: this.selectedFungsi._id,
       namaFungsi: this.selectedFungsi?.nama + ' - ' + this.selectedFungsi.organisasi.nama + (this.selectedFungsi.organisasi.tipe == 'wilayah'? ' - ' + this.selectedFungsi.organisasi.zona.nama : ''),
     });
@@ -71,6 +76,19 @@ export class CetakPage {
 
   selectFungsiDidDismiss() {
     this.selectedFungsi = null;
+  }
+
+  selectKategori(modal) {
+    if (!this.selectedKategori?._id) return;
+    this.formFilterCetak.patchValue({
+      kategori: this.selectedKategori._id,
+      namaKategori: this.selectedKategori?.nama,
+    });
+    modal.dismiss()
+  }
+
+  selectKategoriDidDismiss() {
+    this.selectedKategori = null;
   }
 
   selectDateChange(e){
@@ -83,7 +101,7 @@ export class CetakPage {
   selectDate(modal){
     if (!this.selectedDate.from) return
     modal.dismiss();
-    this.formFilterCetakFungsi.patchValue({
+    this.formFilterCetak.patchValue({
       tanggalStart: this.selectedDate.from,
       tanggalEnd: this.selectedDate.to,
       tanggalCombine: this.selectedDate.from + (this.selectedDate.to? ' - ' + this.selectedDate.to : '')
@@ -93,27 +111,26 @@ export class CetakPage {
   deleteSelectedDate(modal) {
     modal.dismiss();
     this.selectedDate = { from: null, to: null };
-    this.formFilterCetakFungsi.patchValue({ tanggalStart: null, tanggalEnd: null, tanggalCombine: null})
+    this.formFilterCetak.patchValue({ tanggalStart: null, tanggalEnd: null, tanggalCombine: null})
   }
 
-  submitFilter(){
-    // TODO: request permintaan dengan filter
-    console.log('ambil permintaan')
-  }
-
-  async printPermintaan(){
+  async submitFilter(){
     let loading = await this.loading.create({
       mode: 'ios',
       message: 'Memuat data permintaan ' + this.selectedFungsi?.nama
     });
     loading.present();
-    this.cetak.getPermintaan({ fungsi: this.selectedFungsi._id })
+    this.cetak.getPermintaan(this.formFilterCetak.value)
     .subscribe(res => {
       console.log(res)
+      this.dataPermintaan = res.permintaan;
       loading.dismiss();
     }, err => {
       console.log(err);
       loading.dismiss();
     })
+  }
+
+  async printPermintaan(){
   }
 }
