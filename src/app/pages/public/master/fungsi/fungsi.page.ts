2fc2df9347c6a7fa88a5c10f0a34dec694a3248a
mfnizam <fatikhunnizam@gmail.com>
2023-01-09 00:25:40 +0700

    add search functionality on "Pilih Penyetuju" modal

--- src/app/pages/public/master/fungsi/fungsi.page.ts
+++ src/app/pages/public/master/fungsi/fungsi.page.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { AlertController, LoadingController } from '@ionic/angular';
 import { User } from 'app/services/user/user.service';
+import { Subject } from 'rxjs';
+import { debounceTime, takeUntil } from 'rxjs/operators';
 import { Wilayah } from '../wilayah/wilayah.types';
 import { Zona } from '../zona/zona.types';
 import { FungsiService } from './fungsi.service';
@@ -9,10 +16,9 @@ import { Fungsi, Organisasi } from './fungsi.types';
 
 @Component({
   selector: 'app-fungsi',
-  templateUrl: './fungsi.page.html'
+  templateUrl: './fungsi.page.html',
 })
 export class FungsiPage {
-
   loadingDataFungsi = true;
   dataFungsi: Fungsi[] = [];
   loadingDataSelectOrganisasi = false;
@@ -32,8 +38,11 @@ export class FungsiPage {
     ttdPenyetuju: [null],
     penyetuju: [null],
     dataPenyetuju: [null],
-  })
+  });
   loadingCreateUpdateFungsi = false;
+
+  formSearch: FormControl = this.formBuilder.control('');
+  private _unsubscribeAll: Subject<any> = new Subject<any>();
 
   idItemAction: string;
 
@@ -45,23 +54,51 @@ export class FungsiPage {
     private formBuilder: FormBuilder,
     private alert: AlertController,
     private loading: LoadingController
-  ) { }
+  ) {}
 
   ionViewDidEnter() {
-    this.getFungsi()
+    this.getFungsi();
+    this.formSearch.valueChanges
+      .pipe(takeUntil(this._unsubscribeAll), debounceTime(200))
+      .subscribe((value) => {
+        this.loadingDataSelectUser = true;
+        this.fungsi
+          .getUser({
+            ...(value ? { namaLengkap: { $regex: value, $options: 'i' } } : {}),
+          })
+          .subscribe(
+            (res) => {
+              console.log(res);
+              this.loadingDataSelectUser = false;
+              this.dataSelectUser = res.user;
+            },
+            (err) => {
+              console.log(err);
+              this.loadingDataSelectUser = false;
+            }
+          );
+        // this.dataSelectUser = this.dataSelectUser.filter(user => user.namaLengkap.includes(value))
+      });
+  }
+
+  ngOnDestroy(): void {
+    this._unsubscribeAll.next();
+    this._unsubscribeAll.complete();
   }
 
   getFungsi() {
     this.loadingDataFungsi = true;
-    this.fungsi.getFungsi({})
-      .subscribe(res => {
-        console.log(res)
+    this.fungsi.getFungsi({}).subscribe(
+      (res) => {
+        console.log(res);
         this.loadingDataFungsi = false;
-        this.dataFungsi = res.fungsi
-      }, err => {
-        console.log(err)
+        this.dataFungsi = res.fungsi;
+      },
+      (err) => {
+        console.log(err);
         this.loadingDataFungsi = false;
-      })
+      }
+    );
   }
 
   createUpdateFungsi(modal) {
@@ -71,40 +108,45 @@ export class FungsiPage {
 
     let value = this.formCreateUpdateFungsi.value;
     if (value._id) {
-      this.fungsi.updateFungsi(value)
-        .subscribe(res => {
-          console.log(res)
+      this.fungsi.updateFungsi(value).subscribe(
+        (res) => {
+          console.log(res);
           this.loadingCreateUpdateFungsi = false;
           this.getFungsi();
           modal.dismiss();
-        }, err => {
-          console.log(err)
+        },
+        (err) => {
+          console.log(err);
           this.loadingCreateUpdateFungsi = false;
-        })
+        }
+      );
     } else {
-      this.fungsi.createFungsi(value)
-        .subscribe(res => {
-          console.log(res)
+      this.fungsi.createFungsi(value).subscribe(
+        (res) => {
+          console.log(res);
           this.loadingCreateUpdateFungsi = false;
           this.getFungsi();
           modal.dismiss();
-        }, err => {
-          console.log(err)
+        },
+        (err) => {
+          console.log(err);
           this.loadingCreateUpdateFungsi = false;
-        })
+        }
+      );
     }
   }
 
-  ttdAtasanChange(e){
-    console.log(e.detail.checked)
-    if(!e.detail.checked) this.formCreateUpdateFungsi.patchValue({
-      atasan: null,
-      dataAtasan: null
-    })
+  ttdAtasanChange(e) {
+    console.log(e.detail.checked);
+    if (!e.detail.checked)
+      this.formCreateUpdateFungsi.patchValue({
+        atasan: null,
+        dataAtasan: null,
+      });
   }
 
   modalCreateUpdateFungsiDidDismiss() {
-    this.formCreateUpdateFungsi.reset()
+    this.formCreateUpdateFungsi.reset();
   }
 
   itemAction(modal, idItem) {
@@ -115,17 +157,21 @@ export class FungsiPage {
   async actionUpdateItem(modalItemAction, modalCreateUpdateFungsi, id) {
     if (modalItemAction) await modalItemAction.dismiss();
     modalCreateUpdateFungsi.present();
-    let fungsi: Fungsi = this.dataFungsi.find(v => v._id == id)
-    if(!fungsi) return; // TODO: show toast
+    let fungsi: Fungsi = this.dataFungsi.find((v) => v._id == id);
+    if (!fungsi) return; // TODO: show toast
     this.formCreateUpdateFungsi.patchValue({
       ...fungsi,
       organisasi: fungsi.organisasi?._id,
-      namaOrganisasi: fungsi.organisasi?.nama + (fungsi.organisasi.tipe == 'wilayah' ? ' - ' + fungsi.organisasi.zona.nama : ''),
+      namaOrganisasi:
+        fungsi.organisasi?.nama +
+        (fungsi.organisasi.tipe == 'wilayah'
+          ? ' - ' + fungsi.organisasi.zona.nama
+          : ''),
       atasan: fungsi.atasan?._id,
       dataAtasan: fungsi.atasan,
       penyetuju: fungsi.penyetuju?._id,
-      dataPenyetuju: fungsi.penyetuju
-    })
+      dataPenyetuju: fungsi.penyetuju,
+    });
   }
 
   async actionDeleteItem(modal, id) {
@@ -133,29 +179,34 @@ export class FungsiPage {
     let alert = await this.alert.create({
       header: 'Apakah anda yakin ingin menghapus data fungsi ini?',
       mode: 'ios',
-      buttons: [{ text: 'Batal', role: 'cancel' }, { text: 'Hapus', role: 'ok' }]
-    })
+      buttons: [
+        { text: 'Batal', role: 'cancel' },
+        { text: 'Hapus', role: 'ok' },
+      ],
+    });
     await alert.present();
     let { role } = await alert.onDidDismiss();
     if (role != 'ok') return;
     let loading = await this.loading.create({
       message: 'Menghapus fungsi..',
-      mode: 'ios'
-    })
+      mode: 'ios',
+    });
     loading.present();
 
-    this.fungsi.deleteFungsi(id)
-      .subscribe(res => {
-        console.log(res)
+    this.fungsi.deleteFungsi(id).subscribe(
+      (res) => {
+        console.log(res);
         loading.dismiss();
         modal.dismiss();
         this.getFungsi();
-      }, async err => {
+      },
+      async (err) => {
         loading.dismiss();
         console.log(err);
         // modal.dismiss();
         // TODO: add toast
-      })
+      }
+    );
   }
 
   modalActionDidDismiss() {
@@ -165,95 +216,106 @@ export class FungsiPage {
   openModalSelectOrganisasi(modal) {
     modal.present();
     this.loadingDataSelectOrganisasi = true;
-    this.fungsi.getOrganisasi({})
-      .subscribe(res => {
-        console.log(res)
+    this.fungsi.getOrganisasi({}).subscribe(
+      (res) => {
+        console.log(res);
         this.loadingDataSelectOrganisasi = false;
         this.dataSelectOrganisasi = res.organisasi;
-        this.selectedOrganisasi = res.organisasi.find(v => v._id == this.formCreateUpdateFungsi.get('organisasi').value)
-      }, err => {
-        console.log(err)
+        this.selectedOrganisasi = res.organisasi.find(
+          (v) => v._id == this.formCreateUpdateFungsi.get('organisasi').value
+        );
+      },
+      (err) => {
+        console.log(err);
         this.loadingDataSelectOrganisasi = false;
-      })
+      }
+    );
   }
 
   selectOrganisasi(modal) {
     if (!this.selectedOrganisasi?._id) return;
     this.formCreateUpdateFungsi.patchValue({
       organisasi: this.selectedOrganisasi._id,
-      namaOrganisasi: this.selectedOrganisasi?.nama + (this.selectedOrganisasi.tipe == 'wilayah' ? ' - ' + (this.selectedOrganisasi as Wilayah).zona.nama : '')
-    })
-    modal.dismiss()
+      namaOrganisasi:
+        this.selectedOrganisasi?.nama +
+        (this.selectedOrganisasi.tipe == 'wilayah'
+          ? ' - ' + (this.selectedOrganisasi as Wilayah).zona.nama
+          : ''),
+    });
+    modal.dismiss();
   }
 
   selectOrganisasiDidDismiss() {
     this.selectedOrganisasi = null;
   }
 
-  openModalSelectAtasan(modal){
+  openModalSelectAtasan(modal) {
     modal.present();
     this.loadingDataSelectUser = true;
-    this.fungsi.getUser({})
-    .subscribe(res => {
-      console.log(res)
-      this.loadingDataSelectUser = false;
-      this.dataSelectUser = res.user;
-    }, err => {
-      console.log(err)
-      this.loadingDataSelectUser = false;
-    })
+    this.fungsi.getUser({}).subscribe(
+      (res) => {
+        console.log(res);
+        this.loadingDataSelectUser = false;
+        this.dataSelectUser = res.user;
+      },
+      (err) => {
+        console.log(err);
+        this.loadingDataSelectUser = false;
+      }
+    );
   }
 
-  selectAtasan(modal){
+  selectAtasan(modal) {
     modal.dismiss();
     this.formCreateUpdateFungsi.patchValue({
       atasan: this.selectedUser._id,
-      dataAtasan: this.selectedUser
-    })
+      dataAtasan: this.selectedUser,
+    });
   }
 
-  deleteAtasan(){
+  deleteAtasan() {
     this.formCreateUpdateFungsi.patchValue({
       atasan: null,
-      dataAtasan: null
-    })
+      dataAtasan: null,
+    });
   }
 
-  modalSelectAtasanDidDismiss(){
-    this.selectedUser = null
+  modalSelectAtasanDidDismiss() {
+    this.selectedUser = null;
   }
 
-  openModalSelectPenyetuju(modal){
+  openModalSelectPenyetuju(modal) {
     modal.present();
     this.loadingDataSelectUser = true;
-    this.fungsi.getUser({})
-    .subscribe(res => {
-      console.log(res)
-      this.loadingDataSelectUser = false;
-      this.dataSelectUser = res.user;
-    }, err => {
-      console.log(err)
-      this.loadingDataSelectUser = false;
-    })
+    this.fungsi.getUser({}).subscribe(
+      (res) => {
+        console.log(res);
+        this.loadingDataSelectUser = false;
+        this.dataSelectUser = res.user;
+      },
+      (err) => {
+        console.log(err);
+        this.loadingDataSelectUser = false;
+      }
+    );
   }
 
-  selectPenyetuju(modal){
+  selectPenyetuju(modal) {
     modal.dismiss();
     this.formCreateUpdateFungsi.patchValue({
       penyetuju: this.selectedUser._id,
-      dataPenyetuju: this.selectedUser
-    })
+      dataPenyetuju: this.selectedUser,
+    });
   }
 
-  deletePenyetuju(){
+  deletePenyetuju() {
     this.formCreateUpdateFungsi.patchValue({
       penyetuju: null,
-      dataPenyetuju: null
-    })
+      dataPenyetuju: null,
+    });
   }
 
-  selectPenyetujuDidDismiss(){
-    this.selectedUser = null
+  selectPenyetujuDidDismiss() {
+    this.selectedUser = null;
   }
-
 }
